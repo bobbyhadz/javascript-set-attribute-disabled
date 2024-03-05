88a8ab106fa251d6de58527cb1f75b893660c56f
bobbyhadz <opmftw@gmail.com>
2024-03-05 07:04:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const button = document.getElementById('btn');
+
+// ✅ Set the disabled attribute
+button.setAttribute('disabled', '');
+
+// ✅ Remove the disabled attribute
+// button.removeAttribute('disabled');
